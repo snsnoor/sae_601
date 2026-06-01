62ccdc5f0381174b4bac8a7a9d047387f4d9d892
Quentin Ezanno <e2300552@ENS.UNIV-UBS.FR>
2026-06-01 16:18:16 +0200

    Creation application streamlit

---
+++ Dashboard/back.py
@@ -0,0 +1,9 @@
+# back.py
+
+def get_donnees_globales():
+    """Fonction qui gérera l'analyse globale du marché."""
+    return "Les données globales du marché immobilier seront affichées ici."
+
+def get_modele_estimation():
+    """Fonction qui gérera l'estimation de prix."""
+    return "Le formulaire et les résultats d'estimation seront affichés ici."
